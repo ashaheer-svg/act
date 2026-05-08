41c44c0338529368ff5f1ff4d57d0d031ed71114
ashaheer-svg <ashaheer@gmail.com>
2026-05-08 07:49:09 +0530

    style: global rebranding to Activity

--- customers.php
+++ customers.php
@@ -32,7 +32,7 @@ $customers = $db->getCustomerProfiles();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Customer Management - Sales BI</title>
+    <title>Customer Management - Activity</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
